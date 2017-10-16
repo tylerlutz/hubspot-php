5eba139aadb946eb7f0c2f9899214577ee38b815
Ryan Winchester <code@ryanwinchester.ca>
2017-10-16 12:57:28 -0700

    docblocks

--- src/Endpoints/OAuth2.php
+++ src/Endpoints/OAuth2.php
@@ -9,28 +9,69 @@ final class OAuth2 extends Endpoint
     /**
      * Initiate an Integration with OAuth 2.0
      *
+     * In order to initiate OAuth access for your HubSpot App, you'll first
+     * need to send a HubSpot user to an authorization page, where that user
+     * will need to grant access to your app.  When your app sends a user to
+     * that authorization page, you'll use the query parameters detailed below
+     * to identify your app, and to also specify the scopes that your apps
+     * requires.
+     *
+     * Initiating an OAuth connection requires that you create a HubSpot app.
+     * The client ID that you'll need to include in the authorization URL can be
+     * found in the settings for the app, which you can get to by clicking the
+     * name of your app from your developer dashboard.
+     *
+     * Users must be signed into HubSpot to grant access, so any user that is
+     * not logged into HubSpot will be directed to a login screen before being
+     * directed back to the authorization page. The authorization screen will
+     * show the details for your app, and the permissions being requested
+     * (based on the scopes you include in the URL). Users will have the
+     * option to select the Hub ID for the account they wish to grant access to.
+     *
+     * After the user grants access, they will be redirected to the redirect_uri
+     * that you specified, with a code query parameter appended to the URL.
+     * You'll use that code to get an access token from HubSpot.
+     *
+     * @see https://developers.hubspot.com/docs/methods/oauth2/initiate-oauth-integration
+     *
      * @param string $clientId The Client ID of your app.
-     * @param string $redirectURI The URL that you want the visitor redirected to after granting access to your app. For security reasons, this URL must use https.
-     * @param array $scopesArray A set of scopes that your app will need access to.
+     * @param string $redirectURI The URL that you want the visitor redirected
+     *     to after granting access to your app. For security reasons, this URL
+     *     must use https.
+     * @param array $scope A space separated set of scopes that your app will
+     *     need access to. Scopes listed in this parameter will be treated as
+     *     required for your app, and the user will see an error if they select
+     *     an account that does not have access to the scope you've included.
+     * @param array $optionalScope Optional scopes will be automatically dropped
+     *     from the authorization request if the user selects a HubSpot account
+     *     that does not have access to that tool (such as requesting the social
+     *     scope on a CRM only portal).
+     *
      * @return string
      */
-    function getAuthUrl($clientId, $redirectURI, $scopesArray = [])
+    function getAuthUrl($clientId, $redirectURI, $scope = [], $optionalScope = [])
     {
-        $scopeString = '';
-
-        if (count($scopesArray) > 0) {
-            $scopeString = '';
-
-            foreach ($scopesArray as $_index => $scopeStr) {
-                if ($_index > 0) {
-                    $scopeString .= "%20";
-                }
-
-                $scopeString .= $scopeStr;
-            }
+        if (is_array($scope)) {
+            $scope = implode('%20', $scope);
         }
 
-        return "https://app.hubspot.com/oauth/authorize?client_id={$clientId}&scope={$scopeString}&redirect_uri=" . urlencode($redirectURI);
+        if (! empty($optionalScope)) {
+            if (is_array($optionalScope)) {
+                $optionalScope = implode('%20', $scope);
+            }
+
+            $optionalScope = ['optional_scope' => $optionalScope];
+        }
+
+        $params = array_merge($optionalScope, [
+            'client_id' => $clientId,
+            'scope' => $scope,
+            'redirect_uri' => $redirectURI,
+        ]);
+
+        $queryString = build_query_string($params);
+
+        return 'https://app.hubspot.com/oauth/authorize'.$queryString;
     }
 
     /**
